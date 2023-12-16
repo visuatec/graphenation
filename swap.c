3a26ea5a70af059504ededc30a43109a270a083f
Michael Kamaratos <michael@kamaratos.net>
2023-12-16 14:50:09 +0700

    initial commit

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int x = 20;
+  int y = 30;
+
+  printf("x = %i\n", x);  
+  printf("y = %i\n", y);
+
+  // swapping
+
+
+  printf("x is now = %i\n", x);  
+  printf("y is now = %i\n", y);
+}
